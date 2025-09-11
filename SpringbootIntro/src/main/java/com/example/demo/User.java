bb0aef3caa3d723cfbb0b98bf03f716aa098499c
DESKTOP-8CDMI80\oisin <L00108790@student.lyit.ie>
2025-09-11 17:33:55 +0100

    Moved to more stable/supported versions of springboot and java

--- SpringbootIntro/src/main/java/com/example/demo/User.java
+++ SpringbootIntro/src/main/java/com/example/demo/User.java
@@ -13,10 +13,11 @@ public class User {
     private String name;
     private String email;
 
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
